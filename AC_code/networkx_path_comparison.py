415970cf0f62f45b8fcfc615bf8cfcb411dc3827
achase206 <alexmchase@gmail.com>
2025-11-22 23:36:40 -0800

    Added A* implementation

---
+++ AC_code/networkx_path_comparison.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import numpy as np
+from networkx.algorithms.shortest_paths import astar_path, dijkstra_path
+from time import time
+import sys
+import os
+
+current_dir = os.path.dirname(__file__)
+sys.path.append(current_dir)
+
+from graph_helpers import (
+    create_chromo_graph,
+    get_peak_distance_heuristic,
+    print_path,
+    A_star,
+)
+
+
+print("Loading Dataframes...")
+try:
+    chromo_df = pd.read_pickle(f"{current_dir}/chromo_db_filtered.pkl")
+except FileNotFoundError as e:
+    raise FileNotFoundError(
+        "Please add the chromo_db_filtered.pkl file to this directory"
+    )
+
+try:
+    overlap_df = pd.read_pickle(f"{current_dir}/percent_overlap_df.pkl")
+except FileNotFoundError as e:
+    raise FileNotFoundError(
+        "Please add the percent_overlap_df.pkl file to this directory"
+    )
+overlap_df = overlap_df.dropna()
+
+
+print("Constructing Graphs...")
+# Construct a dictionary of FRET closeness graphs for each solvent
+solvent_graphs = {}
+for solvent in overlap_df["Solvent"].unique():
+    solvent_graphs[solvent] = create_chromo_graph(overlap_df, solvent)
+
+# Add -ln(overlap) as our edge weights for pathfinding
+for graph in solvent_graphs.values():
+    for edge in graph.edges(data=True):
+        edge[2]["weight"] = -np.log(edge[2]["Percent Overlap"])
+
+print("Done!")
+print()
+print("-" * 30)
+print("#" * 30)
+print("-" * 30)
+print()
+
+"Solvent Graph Sizes: "
+print("-" * 30)
+print({name: len(graph) for name, graph in solvent_graphs.items()})
+print()
+print("-" * 30)
+print("#" * 30)
+print("-" * 30)
+print()
+
+# Test A* between two random donor/acceptors in chloroform
+solvent = "chloroform"
+start_donor = "O=C1NC(=O)c2ccc(N3CCCCC3)c3cccc1c23"
+end_acceptor = "COc1ccc(-c2cc3c(s2)=CC2=C(C(F)(F)F)c4cc5sc(-c6ccc(OC)cc6)cc5n4[B-](F)(F)[N+]=32)cc1"
+
+heuristic_function = get_peak_distance_heuristic(solvent, chromo_df)
+
+start = time()
+output_path_astar = astar_path(
+    solvent_graphs[solvent],
+    start_donor,
+    end_acceptor,
+    heuristic_function,
+    weight="weight",
+)
+stop = time()
+print("Path Found by A*: ")
+print_path(output_path_astar, solvent_graphs[solvent])
+print(f"runtime: {stop-start:.3f}")
+
+print("-" * 30)
+print()
+
+# Compare with dikstra's
+start = time()
+output_path_dijkstra = dijkstra_path(
+    solvent_graphs[solvent], start_donor, end_acceptor, weight="weight"
+)
+stop = time()
+print("Path Found by Dijkstra's: ")
+print_path(output_path_dijkstra, solvent_graphs[solvent])
+print(f"runtime: {stop-start:.3f}")
+
+print("-" * 30)
+print()
+
+start = time()
+output_path_astar_custom = A_star(
+    solvent_graphs[solvent],
+    start_donor,
+    end_acceptor,
+    heuristic_function,
+)
+stop = time()
+print("Path Found by A* (custom): ")
+print_path(output_path_astar_custom, solvent_graphs[solvent])
+print(f"runtime: {stop-start:.3f}")
